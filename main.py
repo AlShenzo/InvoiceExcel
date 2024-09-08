fa5191ee81520d032f85139929b9988af9822d31
alans <alanshenjc@gmail.com>
2024-09-08 16:50:25 +0300

    added a date

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ for filepath in filepaths:
     # we can create pdf file
     pdf = FPDF(orientation='p', unit='mm', format='A4')
     pdf.add_page()
+
     filename = Path(filepath).stem
     # we import pathlib with Path, we extract the Path() which gives us the file name in form of invoice/filename
     # we then put .stem to isolate the filename
-    invoice_nr = filename.split('-')[0]
+    invoice_nr, date = filename.split('-')
     # when we split we get [10001, 2023.1.18]
     # we then put [0] to get 10001
+    # we could duplicate and write
+    # date = filename.split('-')[1] as separate and keep the invoice [0]
+    # but we can write it as above.
+
     pdf.set_font(family='Times', size=16, style='B')
-    pdf.cell(w=50, h=8, txt=f'Invoice nr.{invoice_nr}')
+    pdf.cell(w=50, h=8, txt=f'Invoice nr.{invoice_nr}', ln=1)
+    # ln=1 to create a break line
+    pdf.set_font(family='Times', size=16, style='B')
+    pdf.cell(w=50, h=8, txt=f'Date: {date}')
+
     pdf.output(f"PDFs/{filename}.pdf")
